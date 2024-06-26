3514614b45d04eeb65729ffd225882f31213f50a
Nur Hadi Saputra <nurhadisaputra7@gmail.com>
2024-06-26 07:18:09 +0700

    model relasi database

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,8 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Employee extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'gender',
+        'age',
+        'phone',
+        'photo',
+        'role_id',
+        'team_id',
+        'is_verified',
+        'verified_at',
+    ];
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class, 'team_id', 'id');
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class, 'role_id', 'id');
+    }
 }
